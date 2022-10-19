5e3bf53129868230dc4a6c7242f8d4da7ea4839e
mbencomo2 <matt.ibencomo@gmail.com>
2022-10-19 01:45:45 -0700

    fixed formatting and styling

--- week06/js/aspCalc.js
+++ week06/js/aspCalc.js
@@ -29,9 +29,11 @@ const aspCalc = {
       value: [21, 9]
     }],
   init: function () {
+    // set the common Aspect ratios and fill the inputs
     setCommonRatios(this.commonRatios);
     setAspectRatio(this.commonRatios);
 
+    // create the event listeners for each element
     document.getElementById('preset').addEventListener('change', aspCalc.setAspects);
     document.getElementById('submitButton').addEventListener('click', aspCalc.updateImg);
     document.getElementById('pixelHeight').addEventListener('input', aspCalc.updateWidth);
@@ -39,39 +41,51 @@ const aspCalc = {
     document.getElementById('aspectHeight').addEventListener('input', aspCalc.updateHeight);
     document.getElementById('aspectWidth').addEventListener('input', aspCalc.updateWidth);
 
+    // set the image size
+    updateImage(document.getElementById('pixelWidth').value, document.getElementById('pixelHeight').value)
   },
   setAspects: function () {
+    // update the aspect ratio to the selected ratio
     setAspectRatio(aspCalc.commonRatios);
   },
   updateImg: function () {
+    // get the values we need
     const h = document.getElementById('pixelHeight').value;
     const w = document.getElementById('pixelWidth').value;
 
+    // set the image size
     updateImage(w, h);
   },
   updateHeight: function () {
+    // get the values we need
     const h = document.getElementById('pixelHeight');
     const w = document.getElementById('pixelWidth');
     const ah = document.getElementById('aspectHeight');
     const aw = document.getElementById('aspectWidth');
 
+    //set the height input value
     h.value = calcHeight(w.value, aw.value, ah.value);
   },
   updateWidth: function () {
+    // get the values we need
     const h = document.getElementById('pixelHeight');
     const w = document.getElementById('pixelWidth');
     const ah = document.getElementById('aspectHeight');
     const aw = document.getElementById('aspectWidth');
 
+    // set the width input value
     w.value = calcWidth(h.value, aw.value, ah.value);
   }
 }
 
 function setCommonRatios(array) {
-  const select = document.getElementById('preset')
+  // define the document element selector
+  const select = document.getElementById('preset');
+  
+  // iterate through each element in the array, and acreate a new option for each
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
-
+    
     const option = document.createElement('option');
     option.value = element.name;
     option.textContent = `${element.name} - ${element.value[0]}:${element.value[1]}`;
@@ -80,10 +94,12 @@ function setCommonRatios(array) {
 }
 
 function setAspectRatio(array) {
+  // define the document elements we need
   const w = document.getElementById('aspectWidth');
   const h = document.getElementById('aspectHeight');
   const selector = document.getElementById('preset');
 
+  // iterate through the array, finding the current option and applying the values
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
 
@@ -93,21 +109,23 @@ function setAspectRatio(array) {
     }
   }
 
+  // set default values for the inputs
   document.getElementById('pixelWidth').value = w.value * 120;
   document.getElementById('pixelHeight').value = h.value * 120;
-
-  aspCalc.updateImg(w.value, h.value);
 }
 
 function updateImage(w, h) {
+  // define the document elements we need
   const fakeImg = document.getElementById('fakeImg');
   const caption = document.getElementById('fakeImgSize');
   const warning = document.getElementById('warning');
+  //get the width of the 'main' element
   const mainWidth = document.querySelector('main').clientWidth;
 
+  // if the width the user input is larget than the width of 'main, scale the image accordingly
   if (w > mainWidth) {
     fakeImg.setAttribute("style", `width:${w*(mainWidth/w)}px; height: ${h*(mainWidth/w)}px;`);
-    warning.textContent = 'Image size too big to accurately display';
+    warning.textContent = 'Image has been scaled to fit appropriately';
   }
   else {
     fakeImg.setAttribute("style", `width:${w}px; height: ${h}px;`);
@@ -116,6 +134,7 @@ function updateImage(w, h) {
   caption.textContent = `${w} x ${h}`;
 }
 
+// simple algebra to find the height or width
 function calcWidth(h, aw, ah) { return Math.round((aw * h) / ah) }
 function calcHeight(w, aw, ah) { return Math.round((w * ah) / aw) }
 
